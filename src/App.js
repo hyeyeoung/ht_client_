167c550bd1e292b96808292dfab1fcc8ef75334b
hy <rlagpdud0524@gmail.com>
2023-07-31 21:44:26 +0900

    join

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
-// import logo from './logo.svg';
-// import './App.css';
+// src/App.js (리액트 앱 파일)
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//App.js
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import React,{useState,useEffect} from 'react';
 
 function App() {
+  const [data, setData] = useState({});
 
-  const sendRequest = async() => {
-    const response = await axios.get('https://port-0-ht-test-eu1k2llkqouw2h.sel4.cloudtype.app/');
-    console.log(response);
-    console.log(response.data);
-  };
-
-  useEffect(()=>{
-    sendRequest();    
-  });
+  useEffect(() => {
+    axios.get('https://port-0-ht-test-eu1k2llkqouw2h.sel4.cloudtype.app//api/data') // Express 서버의 포트 번호를 여기에 기입
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('API 요청에 실패했습니다:', error);
+      });
+  }, []);
 
   return (
-    <div className="App">      
+    <div>
+      <h1>{data.message}</h1>
     </div>
   );
 }
+
+export default App;
